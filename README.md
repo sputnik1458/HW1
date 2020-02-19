77462c352f5e45a61a7e3ff7b705a9d96d036931
Jacob Frank <jacobf1458@protonmail.com>
2020-02-18 22:01:08 -0500

    Part 1

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # HW1
+## Part 1
+`g++ -o hw1-1 hw1-1.cpp`
+`./hw1-1 n a c g t k p outfile`
+
+* *n* (**int**): sequence length
+* *a c g t* (**int**): fraction of letters A, C, G, and T
+* *k* (**int**): number of sequences
+* *p* (**float** [0:1]): mutation probability
+* *outfile* (**string**): output file
---
+++ hw1-1.cpp
@@ -0,0 +1,73 @@
+/* CIS4930 - Data Science in Bioinformatics Spring 2020
+  University of Florida
+  HW1
+  Jacob Frank & Pedro Sicilia
+*/
+
+#include <iostream>
+#include <random>
+#include <ctime>
+#include <cstdlib>
+#include <cstring>
+#include <fstream>
+
+// returns a random nucleotide based on given probabilities
+char genNucleotide(int a, int c, int g, int s) {
+  int r = std::rand() % s + 1;
+
+  if (r < a) {
+      return 'A';
+  } else if (r < a+c) {
+      return 'C';
+  } else if (r < a+c+g) {
+      return 'G';
+  } else {
+      return 'T';
+  }
+}
+
+int main(int argc, char* argv[]) {
+
+  int n = std::atoi(argv[1]); // sequence length
+  int a = std::atoi(argv[2]);
+  int c = std::atoi(argv[3]);
+  int g = std::atoi(argv[4]);
+  int t = std::atoi(argv[5]);
+  int s = a + c + g + t;
+
+  int k = std::atoi(argv[6]); // # sequences
+  float p = std::atof(argv[7]); // mutation prob
+
+  std::ofstream outfile;
+  outfile.open(argv[8]);
+
+  std::srand((unsigned)std::time(0));
+
+  char nt;
+  char f[n];
+
+  // generates first sequence
+  for (int i = 0; i < n; i++) {
+    nt = genNucleotide(a, c, g, s);
+    f[i] = nt;
+    outfile << nt;
+  }
+  outfile << '\n';
+
+  // generates k-1 sequences with mutation probability
+  for (int j = 0; j < k-1; j++) {
+    for (int i = 0; i < n; i++) {
+      bool mut = std::rand() % 1000 + 1 < p*1000;
+      if (mut) {
+        outfile << genNucleotide(a, c, g, s);
+      } else {
+        outfile << f[i];
+      }
+    }
+    outfile << '\n';
+  }
+  outfile.close();
+
+  return 0;
+
+}
